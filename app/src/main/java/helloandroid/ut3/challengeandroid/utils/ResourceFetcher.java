b7fe00d75aff473843b550ec07cd15634886974e
Oriol Camps Pérez <oriol.camps-perez@univ-tlse3.fr>
2024-03-23 15:41:29 +0100

    Add more sprites

--- app/src/main/java/helloandroid/ut3/challengeandroid/utils/ResourceFetcher.java
+++ app/src/main/java/helloandroid/ut3/challengeandroid/utils/ResourceFetcher.java
@@ -10,11 +10,11 @@ import java.util.List;
 import helloandroid.ut3.challengeandroid.R;
 
 public abstract class ResourceFetcher {
-    private static Bitmap convertToBitmap(int ressource, Context context) {
-        return BitmapFactory.decodeResource(context.getResources(), ressource);
+    private static Bitmap convertToBitmap(int resource, Context context) {
+        return BitmapFactory.decodeResource(context.getResources(), resource);
     }
 
-    public static List<Bitmap> getEnemiesBitmap (Context context) {
+    public static List<Bitmap> getEnemiesBitmap(Context context) {
         List<Bitmap> resList = new ArrayList<>();
         resList.add(convertToBitmap(R.drawable.enemypeach1, context));
         resList.add(convertToBitmap(R.drawable.enemypeach2, context));
@@ -23,22 +23,76 @@ public abstract class ResourceFetcher {
         return resList;
     }
 
-    public static List<Bitmap> getGameCharacterBitmap (Context context) {
+    public static List<Bitmap> getGameCharacterRunBitmap(Context context) {
         List<Bitmap> resList = new ArrayList<>();
-        resList.add(convertToBitmap(R.drawable.mainchar, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_run1, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_run2, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_run3, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_run4, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_run5, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_run6, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_run7, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_run8, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_run9, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_run10, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_run11, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_run12, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_run13, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_run14, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_run15, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_run16, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_run17, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_run18, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_run19, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_run20, context));
         return resList;
     }
 
-    public static List<Bitmap> getGhostsBitmap (Context context) {
+    public static List<Bitmap> getGameCharacterJumpBitmap(Context context) {
         List<Bitmap> resList = new ArrayList<>();
-        resList.add(convertToBitmap(R.drawable.waterghost1, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_jump1, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_jump2, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_jump3, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_jump4, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_jump5, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_jump6, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_jump7, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_jump8, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_jump9, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_jump10, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_jump11, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_jump12, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_jump13, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_jump14, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_jump15, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_jump16, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_jump17, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_jump18, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_jump19, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_jump20, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_jump21, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_jump22, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_jump23, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_jump24, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_jump25, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_jump26, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_jump27, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_jump28, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_jump29, context));
+        resList.add(convertToBitmap(R.drawable.maincharacter_jump30, context));
+        return resList;
+    }
+
+    public static List<Bitmap> getGhostsBitmap(Context context) {
+        List<Bitmap> resList = new ArrayList<>();
+        resList.add(convertToBitmap(R.drawable.waterghost1_menu, context));
         resList.add(convertToBitmap(R.drawable.waterghost2, context));
         resList.add(convertToBitmap(R.drawable.waterghost3, context));
         resList.add(convertToBitmap(R.drawable.waterghost4, context));
         return resList;
     }
 
-    public static List<Bitmap> getWallsBitmap (Context context) {
+    public static List<Bitmap> getWallsBitmap(Context context) {
         List<Bitmap> resList = new ArrayList<>();
         resList.add(convertToBitmap(R.drawable.mushroomwall1, context));
         resList.add(convertToBitmap(R.drawable.mushroomwall2, context));
